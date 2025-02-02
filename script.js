cd58fd9cfadabc17c31233f5e44934f527c142f5
MasterMy <seccssi@gmail.com>
2025-02-02 09:48:18 +0300

    Update

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Testing");
+
+let apiQuotes = [];
+// Show new quote
+function newQuote(){
+    // Pick a random quote from apiQuotes array
+   const Quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+   console.log (Quote);
+}
+// Get quotes from API
+async function getQuotes() {
+    const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+    try {
+        response = await fetch (apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }catch (error) {
+        // Catch error here
+    }
+}
+// On load
+getQuotes ();   
+
+    
